f30514d91e73dd36f1e57e336574e8c47da09c4e
Lars de Lange <lars.delange@ampelmann.nl>
2020-07-05 15:30:36 +0200

    Update RaukinCC.lua

--- RaukinCC.lua
+++ RaukinCC.lua
@@ -21,8 +21,9 @@ function RaukinCC_OnLoad()
 		RaukinCC_SavedVar["Seduction"]=true
 		RaukinCC_SavedVar["Sap"]=true
 		RaukinCC_SavedVar["Repentance"]=true
-		RaukinCC_SavedVar["Chastise"]=true
+		RaukinCC_SavedVar["Entangling Roots"]=true
 		RaukinCC_SavedVar["Blind"]=true
+		RaukinCC_SavedVar["Scare Beast"]=true
 	end
 end
 
@@ -31,64 +32,93 @@ function Load_List()
 	Announce_Toggle = false
 
 	if RaukinCC_SavedVar["Fear"] then
-		table.insert(Raukin_CCSpells,"Fear")
+		table.insert(Raukin_CCSpells,5782)
+		table.insert(Raukin_CCSpells,6213)
+		table.insert(Raukin_CCSpells,6215)
 		RaukinCCFrame_CheckButton_CC1:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC1:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Polymorph"] then
-		table.insert(Raukin_CCSpells,"Polymorph")
+		table.insert(Raukin_CCSpells,118)
+		table.insert(Raukin_CCSpells,12824)
+		table.insert(Raukin_CCSpells,12825)
+		table.insert(Raukin_CCSpells,12826)
+		table.insert(Raukin_CCSpells,28272)
+		table.insert(Raukin_CCSpells,28271)
 		RaukinCCFrame_CheckButton_CC2:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC2:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Shackle Undead"] then
-		table.insert(Raukin_CCSpells,"Shackle Undead")
+		table.insert(Raukin_CCSpells,9484)
+		table.insert(Raukin_CCSpells,9485)
+		table.insert(Raukin_CCSpells,10955)
 		RaukinCCFrame_CheckButton_CC3:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC3:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Hibernate"] then
-		table.insert(Raukin_CCSpells,"Hibernate")
+		table.insert(Raukin_CCSpells,2637)
+		table.insert(Raukin_CCSpells,18657)
+		table.insert(Raukin_CCSpells,18658)
 		RaukinCCFrame_CheckButton_CC4:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC4:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Freezing Trap Effect"] then
-		table.insert(Raukin_CCSpells,"Freezing Trap Effect")
+		table.insert(Raukin_CCSpells,3355)
+		table.insert(Raukin_CCSpells,14308)
+		table.insert(Raukin_CCSpells,14309)
 		RaukinCCFrame_CheckButton_CC5:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC5:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Seduction"] then
-		table.insert(Raukin_CCSpells,"Seduction")
+		table.insert(Raukin_CCSpells,6358)
 		RaukinCCFrame_CheckButton_CC6:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC6:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Sap"] then
-		table.insert(Raukin_CCSpells,"Sap")
+		table.insert(Raukin_CCSpells,6770)
+		table.insert(Raukin_CCSpells,2070)
+		table.insert(Raukin_CCSpells,11297)
 		RaukinCCFrame_CheckButton_CC7:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC7:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Repentance"] then
-		table.insert(Raukin_CCSpells,"Repentance")
+		table.insert(Raukin_CCSpells,20066)
 		RaukinCCFrame_CheckButton_CC8:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC8:SetChecked(false)
 	end
-	if RaukinCC_SavedVar["Chastise"] then
-		table.insert(Raukin_CCSpells,"Chastise")
+	if RaukinCC_SavedVar["Entangling Roots"] then
+		table.insert(Raukin_CCSpells,339)
+		table.insert(Raukin_CCSpells,1062)
+		table.insert(Raukin_CCSpells,5195)
+		table.insert(Raukin_CCSpells,5196)
+		table.insert(Raukin_CCSpells,9852)
+		table.insert(Raukin_CCSpells,9853)
+		table.insert(Raukin_CCSpells,26989)
 		RaukinCCFrame_CheckButton_CC9:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC9:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Blind"] then
-		table.insert(Raukin_CCSpells,"Blind")
+		table.insert(Raukin_CCSpells,2094)
 		RaukinCCFrame_CheckButton_CC10:SetChecked(true)
 	else
 		RaukinCCFrame_CheckButton_CC10:SetChecked(false)
+	end
+	if RaukinCC_SavedVar["Scare Beast"] then
+		table.insert(Raukin_CCSpells,1513)
+		table.insert(Raukin_CCSpells,14326)
+		table.insert(Raukin_CCSpells,14327)
+		RaukinCCFrame_CheckButton_CC11:SetChecked(true)
+	else
+		RaukinCCFrame_CheckButton_CC11:SetChecked(false)
 	end
 	if RaukinCC_SavedVar["Announce_Toggle"] then
 		Announce_Toggle = true
@@ -124,64 +154,93 @@ function Refresh_List()
 	Whisper_Toggle = false
 
 	if RaukinCCFrame_CheckButton_CC1:GetChecked() then
-		table.insert(Raukin_CCSpells,"Fear")
+		table.insert(Raukin_CCSpells,5782)
+		table.insert(Raukin_CCSpells,6213)
+		table.insert(Raukin_CCSpells,6215)
 		RaukinCC_SavedVar["Fear"] = true
 	else
 		RaukinCC_SavedVar["Fear"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC2:GetChecked() then
-		table.insert(Raukin_CCSpells,"Polymorph")
+		table.insert(Raukin_CCSpells,118)
+		table.insert(Raukin_CCSpells,12824)
+		table.insert(Raukin_CCSpells,12825)
+		table.insert(Raukin_CCSpells,12826)
+		table.insert(Raukin_CCSpells,12872)
+		table.insert(Raukin_CCSpells,12871)
 		RaukinCC_SavedVar["Polymorph"] = true
 	else
 		RaukinCC_SavedVar["Polymorph"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC3:GetChecked() then
-		table.insert(Raukin_CCSpells,"Shackle Undead")
+		table.insert(Raukin_CCSpells,9484)
+		table.insert(Raukin_CCSpells,9485)
+		table.insert(Raukin_CCSpells,10955)
 		RaukinCC_SavedVar["Shackle Undead"] = true
 	else
 		RaukinCC_SavedVar["Shackle Undead"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC4:GetChecked() then
-		table.insert(Raukin_CCSpells,"Hibernate")
+		table.insert(Raukin_CCSpells,2637)
+		table.insert(Raukin_CCSpells,18657)
+		table.insert(Raukin_CCSpells,18658)
 		RaukinCC_SavedVar["Hibernate"] = true
 	else
 		RaukinCC_SavedVar["Hibernate"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC5:GetChecked() then
-		table.insert(Raukin_CCSpells,"Freezing Trap Effect")
+		table.insert(Raukin_CCSpells,3355)
+		table.insert(Raukin_CCSpells,14308)
+		table.insert(Raukin_CCSpells,14309)
 		RaukinCC_SavedVar["Freezing Trap Effect"] = true
 	else
 		RaukinCC_SavedVar["Freezing Trap Effect"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC6:GetChecked() then
-		table.insert(Raukin_CCSpells,"Seduction")
+		table.insert(Raukin_CCSpells,6358)
 		RaukinCC_SavedVar["Seduction"] = true
 	else
 		RaukinCC_SavedVar["Seduction"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC7:GetChecked() then
-		table.insert(Raukin_CCSpells,"Sap")
+		table.insert(Raukin_CCSpells,6770)
+		table.insert(Raukin_CCSpells,2070)
+		table.insert(Raukin_CCSpells,11297)
 		RaukinCC_SavedVar["Sap"] = true
 	else
 		RaukinCC_SavedVar["Sap"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC8:GetChecked() then
-		table.insert(Raukin_CCSpells,"Repentance")
+		table.insert(Raukin_CCSpells,20066)
 		RaukinCC_SavedVar["Repentance"] = true
 	else
 		RaukinCC_SavedVar["Repentance"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC9:GetChecked() then
-		table.insert(Raukin_CCSpells,"Chastise")
-		RaukinCC_SavedVar["Chastise"] = true
+		table.insert(Raukin_CCSpells,339)
+		table.insert(Raukin_CCSpells,1062)
+		table.insert(Raukin_CCSpells,5195)
+		table.insert(Raukin_CCSpells,5196)
+		table.insert(Raukin_CCSpells,9852)
+		table.insert(Raukin_CCSpells,9853)
+		table.insert(Raukin_CCSpells,26989)
+		RaukinCC_SavedVar["Entangling Roots"] = true
 	else
-		RaukinCC_SavedVar["Chastise"] = false
+		RaukinCC_SavedVar["Entangling Roots"] = false
 	end
 	if RaukinCCFrame_CheckButton_CC10:GetChecked() then
-		table.insert(Raukin_CCSpells,"Blind")
+		table.insert(Raukin_CCSpells,2094)
 		RaukinCC_SavedVar["Blind"] = true
 	else
 		RaukinCC_SavedVar["Blind"] = false
+	end
+	if RaukinCCFrame_CheckButton_CC11:GetChecked() then
+		table.insert(Raukin_CCSpells,1513)
+		table.insert(Raukin_CCSpells,14326)
+		table.insert(Raukin_CCSpells,14327)
+		RaukinCC_SavedVar["Scare Beast"] = true
+	else
+		RaukinCC_SavedVar["Scare Beast"] = false
 	end
 	if RaukinCCFrame_CheckButton_An:GetChecked() then
 		Announce_Toggle = true
@@ -209,94 +268,98 @@ function Refresh_List()
 	end
 end
 
-function RaukinCC_OnEvent(self, event, ...)	
-	if(event == "COMBAT_LOG_EVENT_UNFILTERED") then
+function RaukinCC_OnEvent(self, event, ...)
+	Arena=IsActiveBattlefieldArena("player")
+	Battleground=UnitInBattleground("player")
+	if((event == "COMBAT_LOG_EVENT_UNFILTERED") and (Arena==nil) and (Battleground==nil)) then
 		local timestamp, combatEvent, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags = select(1, ...)
 		if(combatEvent == "SPELL_DAMAGE") then
 			local spellId, spellName, spellSchool = select(9, ...)
 			j = contains(Raukin_MobGUID,destGUID)
-			if j~=false then
-				CCSpellName = Raukin_MobGUID[j].sN
+			if j~=100010001000 then
 				if Raukin_MobGUID[j].sC==false then
-					msg = CCSpellName  .. " on " .. destName .. " is broken by: " .. " " .. sourceName .. " (" .. spellName .. ")"
-					Announce(msg, sourceName, CCSpellName, destName)
-					Raukin_MobGUID[j].sC = true
+					Raukin_MobGUID[j].aN = sourceName
+					Raukin_MobGUID[j].aS = spellName
+					Raukin_MobGUID[j].dN = destName
 				end
-			end	
+			end
 		elseif(combatEvent == "SPELL_PERIODIC_DAMAGE") then
 			local spellId, spellName, spellSchool = select(9, ...)
 			j = contains(Raukin_MobGUID,destGUID)
-			if j~=false then
-				CCSpellName = Raukin_MobGUID[j].sN
+			if j~=100010001000 then
 				if Raukin_MobGUID[j].sC==false then
-					msg = CCSpellName  .. " on " .. destName .. " is broken by: " .. " " .. sourceName .. " (" .. spellName .. ")"
-					Announce(msg, sourceName, CCSpellName, destName)
-					Raukin_MobGUID[j].sC = true
+					Raukin_MobGUID[j].aN = sourceName
+					Raukin_MobGUID[j].aS = spellName
+					Raukin_MobGUID[j].dN = destName
 				end
 			end
  		elseif(combatEvent == "RANGE_DAMAGE") then
 			j = contains(Raukin_MobGUID,destGUID)
-			if j~=false then
-				CCSpellName = Raukin_MobGUID[j].sN
+			if j~=100010001000 then
 				if Raukin_MobGUID[j].sC==false then
-					msg = CCSpellName  .. " on " .. destName .. " is broken by: " .. " " .. sourceName
-					Announce(msg, sourceName, CCSpellName, destName)
-					Raukin_MobGUID[j].sC = true
+					Raukin_MobGUID[j].aN = sourceName
+					Raukin_MobGUID[j].dN = destName
 				end
 			end
 		elseif(combatEvent == "SWING_DAMAGE") then
 			j = contains(Raukin_MobGUID,destGUID)
-			if j~=false then
-				CCSpellName = Raukin_MobGUID[j].sN
+			if j~=100010001000 then
 				if Raukin_MobGUID[j].sC==false then
-					msg = CCSpellName  .. " on " .. destName .. " is broken by: " .. " " .. sourceName
-					Announce(msg, sourceName, CCSpellName, destName)
-					Raukin_MobGUID[j].sC = true
-				end	
+					Raukin_MobGUID[j].aN = sourceName
+					Raukin_MobGUID[j].dN = destName
+				end
 			end
 		elseif(combatEvent == "SPELL_AURA_REMOVED") then
 			local spellId, spellName, spellSchool = select(9, ...)
-			if(CheckCCSpell(Raukin_CCSpells,spellName)) then
+			if(CheckCCSpell(Raukin_CCSpells,spellId)) then
 				j = contains(Raukin_MobGUID,destGUID)
-				if j~=false then
-					if Raukin_MobGUID[j].sC == false then
-						CCSpellName = Raukin_MobGUID[j].sN
-						if Raukin_MobGUID[j].sC==false then	
-							msg = CCSpellName .. " ran out on target: " .. " " .. destName;
-							Announce(msg, sourceName, CCSpellName, destName)
-							Raukin_MobGUID[j].sC = true	
-						end
-					else
+				if j~=100010001000 then
+					if Raukin_MobGUID[j].dN==nil then
+						Raukin_MobGUID[j].dN="Unknown"
+					end
+					if Raukin_MobGUID[j].aN == "None" then
+						msg = Raukin_MobGUID[j].sN .. " ran out on target: " .. " " .. destName;
+						Announce(msg, Raukin_MobGUID[j].aN, Raukin_MobGUID[j].sN, destName)
 						table.remove(Raukin_MobGUID,j)
-					end	
+					else
+						if Raukin_MobGUID[j].aS == "None" then
+							msg = Raukin_MobGUID[j].sN  .. " on " .. Raukin_MobGUID[j].dN .. " is broken by: " .. " " .. Raukin_MobGUID[j].aN
+							Announce(msg, Raukin_MobGUID[j].aN, Raukin_MobGUID[j].sN, Raukin_MobGUID[j].dN)
+							table.remove(Raukin_MobGUID,j)
+						else
+							msg = Raukin_MobGUID[j].sN  .. " on " .. Raukin_MobGUID[j].dN .. " is broken by: " .. " " .. Raukin_MobGUID[j].aN .. " (" .. Raukin_MobGUID[j].aS .. ")"
+							Announce(msg, Raukin_MobGUID[j].aN, Raukin_MobGUID[j].sN, Raukin_MobGUID[j].dN)
+							table.remove(Raukin_MobGUID,j)
+						end
+					end
 				end
 			end
 		elseif(combatEvent == "SPELL_AURA_REFRESH") then
 			local spellId, spellName, spellSchool = select(9, ...)
-			if(CheckCCSpell(Raukin_CCSpells,spellName)) then
+			if(CheckCCSpell(Raukin_CCSpells,spellId)) then
 				if(contains(Raukin_MobGUID,destGUID)>0 and Loadin(isInLineOfSight) and UnitPlayerControlled(destName)==nil) then
 					j = contains(Raukin_MobGUID,destGUID)
 					if Raukin_MobGUID[j].sN~=spellName then
 						table.remove(Raukin_MobGUID,j)
-						table.insert(Raukin_MobGUID,{dG = destGUID,sN = spellName, sC=false})
+						table.insert(Raukin_MobGUID,{dG = destGUID,sN = spellName, sC=false, aN="None", aS="None", dN="None"})
 					end
 				end
 			end
 
 	 	elseif(combatEvent == "SPELL_AURA_APPLIED") then
 			local spellId, spellName, spellSchool = select(9, ...)
- 			if(CheckCCSpell(Raukin_CCSpells,spellName)) then
+ 			if(CheckCCSpell(Raukin_CCSpells,spellId)) then
 				-- DEFAULT_CHAT_FRAME:AddMessage("<RaukinCC>: " .. spellName .. " was casted on " .. destName);
-				-- Register ID 
-				if(contains(Raukin_MobGUID,destGUID)==false and Loadin(isInLineOfSight) and UnitPlayerControlled(destName)==nil) then
-					table.insert(Raukin_MobGUID,{dG = destGUID,sN = spellName, sC=false})
+				-- Register ID
+				if(contains(Raukin_MobGUID,destGUID)==0 and Loadin(isInLineOfSight) and UnitPlayerControlled(destName)==nil) then
+					table.insert(Raukin_MobGUID,{dG = destGUID,sN = spellName, sC=false, aN="None", aS="None"})
 				elseif(contains(Raukin_MobGUID,destGUID)>0 and Loadin(isInLineOfSight) and UnitPlayerControlled(destName)==nil) then
 					j = contains(Raukin_MobGUID,destGUID)
 					table.remove(Raukin_MobGUID,j)
-					table.insert(Raukin_MobGUID,{dG = destGUID,sN = spellName, sC=false})
+					table.insert(Raukin_MobGUID,{dG = destGUID,sN = spellName, sC=false, aN="None", aS="None", dN="None"})
 				end
-				--for i=1,table.getn(Raukin_MobGUID) do  				
-					--print(i,Raukin_MobGUID[i].dG,Raukin_MobGUID[i].sN)
+				--for i=1,table.getn(Raukin_MobGUID) do
+					--print(i,Raukin_MobGUID[i].dG,Raukin_MobGUID[i].aN,Raukin_MobGUID[i].sN)
 				--end
 			end
  		else
@@ -332,12 +395,12 @@ function RaukinCC_SlashCommandHandler()
 end
 
 function contains(data, search)
-	for i=1,table.getn(data) do 
+	for i=1,table.getn(data) do
 		if(data[i].dG==search) then
 			return i
 		end
 	end
-	return false
+	return 100010001000
 end
 
 function CheckCCSpell(data, search)
@@ -392,4 +455,3 @@ function Loadin(Sight)
 	end
 	return true
 end
-
